0c1b1202f354be7b12d2734b7dc74df424309261
Justin Bronn <justin.bronn@maxar.com>
2023-12-20 15:45:01 -0700

    Fix resource for passing instance role permissions.

--- modules/karpenter/main.tf
+++ modules/karpenter/main.tf
@@ -241,7 +241,7 @@ data "aws_iam_policy_document" "pod_identity" {
 
   statement {
     sid       = "AllowPassingInstanceRole"
-    resources = ["arn:${local.partition}:iam::*:role/KarpenterNodeRole-${var.cluster_name}"]
+    resources = [local.create_iam_role ? aws_iam_role.this[0].arn : var.iam_role_arn]
     actions   = ["iam:PassRole"]
 
     condition {
